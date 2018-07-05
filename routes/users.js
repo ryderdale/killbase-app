47a7ed1c1990be128cb7ccdf7a89ab403b1a5d5a
Ryder Dale <rydersdale@gmail.com>
2018-07-05 10:25:03 -0500

    got create account working and user id specific ID to store client side

--- routes/users.js
+++ routes/users.js
@@ -30,21 +30,26 @@ router.post('/', function(req, res) {
     console.log('Request body:', req.body);
     bcrypt.hash(req.body.password, 12)
     .then( (hashed_password) => {
-        let newBody = req.body;
-        delete newBody.password; 
-        newBody.hashed_password = hashed_password;
-        console.log(newBody);
-        return knex('users').insert(newBody);
+        // let newBody = req.body;
+        // delete newBody.password; 
+        // newBody.hashed_password = hashed_password;
+        // console.log(newBody);
+        return knex('users').insert({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            username: req.body.username,
+            email: req.body.email,
+            hashed_password: hashed_password
+        });
     })
-    // .then((newBody) => {
-    //     return knex('users').where('username', newBody.username)
-    // })
-    // .then((user_object)=> {
-    //     res.status(200).send(user_object)
-    // })
+    .then(() => {
+        return knex('users').where('username', req.body.username)
+    })
+    .then((user_object)=> {
+        res.status(200).send(user_object)
+    })
     .catch((error) => {
-        console.error(error);
-        res.status(500).send({ error });
+        res.status(500).send(error);
     });
 });
 
